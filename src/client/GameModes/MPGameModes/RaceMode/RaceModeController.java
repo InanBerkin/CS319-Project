0e2af908a9da7a0b7a20a3ae28c8b1f28867080e
berkininan <berkin.inan1@gmail.com>
2018-12-22 00:07:25 +0300

    Added other players to multiplayer game

--- src/client/GameModes/MPGameModes/RaceMode/RaceModeController.java
+++ src/client/GameModes/MPGameModes/RaceMode/RaceModeController.java
@@ -3,6 +3,7 @@ package client.GameModes.MPGameModes.RaceMode;
 import client.GameModes.GameInstance;
 import client.GameModels.*;
 import client.Menus.RoomLobbyMenu.RoomLobbyMenuController;
+import client.QBitzApplication;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
@@ -21,7 +22,12 @@ public class RaceModeController extends GameInstance {
 
     @Override
     public void onMessageReceived(String message) {
+        JSONObject responseJSON = new JSONObject(message);
+        if(responseJSON.getString("responseType").equals("submit")){
+            Platform.runLater(() -> {
 
+            });
+        }
     }
 
     @Override
@@ -30,27 +36,43 @@ public class RaceModeController extends GameInstance {
         board = new GameBoard(gridDimension, null);
         pattern = new Pattern(gridDimension);
         pattern.setGivenPattern(jsonArrayToMatrix(payload.getJSONArray("patternMatrix"), gridDimension));
+        addPlayers(payload.getJSONArray("userList"));
+    }
+
+    @Override
+    public boolean submit(){
+        boolean isPatternTrue = super.submit();
+        if(isPatternTrue){
+            JSONObject submitJSON = new JSONObject();
+            submitJSON.put("requestType", "submit");
+            submitJSON.put("roomID", payload.getInt("roomID"));
+            submitJSON.put("finishTime", gameTimer.getGameTime().getValue());
+            QBitzApplication.getSceneController().sendMessageToServer(submitJSON);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     private void addPlayers(JSONArray playersList){
-//        playersbar.getChildren().clear();
-//        int players = playersList.length();
-//        Player player;
-//        VBox vBox;
-//        int id;
-//        int level;
-//        String name;
-//        for (int i = 0; i < players; i++){
-//            JSONObject playerJSON = (JSONObject) playersList.get(i);
-//            id = playerJSON.getInt("id");
-//            level = playerJSON.getInt("level");
-//            name = playerJSON.getString("name");
-//            player = new Player(id,level,name);
-//            vBox = new VBox();
-//            vBox.setAlignment(Pos.CENTER_LEFT);
-//            vBox.getChildren().add(new Label(player.getName()));
-//            playersbar.add(,i/4 ,hBox i);
-//        }
+        playersbar.getChildren().clear();
+        int players = playersList.length();
+        Player player;
+        VBox vBox;
+        int id;
+        int level;
+        String name;
+        for (int i = 0; i < players; i++){
+            JSONObject playerJSON = (JSONObject) playersList.get(i);
+            id = playerJSON.getInt("id");
+            name = playerJSON.getString("name");
+            player = new Player(id,name);
+            vBox = new VBox();
+            vBox.setAlignment(Pos.CENTER);
+            vBox.getChildren().add(new Label(player.getName()));
+            playersbar.getChildren().add(vBox);
+        }
     }
 
     private class Player{
@@ -58,10 +80,9 @@ public class RaceModeController extends GameInstance {
         private String name;
         private String time;
 
-        public Player(int id, int level, String name, String time) {
+        public Player(int id, String name) {
             this.id = id;
             this.name = name;
-            this.time = time;
         }
 
         public int getId() {
@@ -74,6 +95,10 @@ public class RaceModeController extends GameInstance {
 
         public String getName() {
             return name;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
         }
     }
 }
