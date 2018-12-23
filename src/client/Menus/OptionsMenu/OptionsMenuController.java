bd3435c5853f0844533d31f122d22bb8d406697e
Abdullah Talayhan <abdullahttj@gmail.com>
2018-12-23 15:03:09 +0300

    CubeFace index Fix

--- src/client/Menus/OptionsMenu/OptionsMenuController.java
+++ src/client/Menus/OptionsMenu/OptionsMenuController.java
@@ -74,7 +74,7 @@ public class OptionsMenuController extends MenuController {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 CubeFaces.changeColor(colorSlider.getValue());
-                faceView.setImage(CubeFaces.getImageAt(0));
+                faceView.setImage(CubeFaces.getImageAt(1));
 
             }
         }
