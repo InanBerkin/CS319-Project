7db14a6b60b11338cd324b797e091ca0d740157c
berkininan <berkin.inan1@gmail.com>
2018-12-23 13:39:52 +0300

    Fixed disabled button in lobby

--- src/client/Menus/RoomLobbyMenu/RoomLobbyMenuController.java
+++ src/client/Menus/RoomLobbyMenu/RoomLobbyMenuController.java
@@ -41,7 +41,7 @@ public class RoomLobbyMenuController extends MenuController {
         if(responseJSON.getString("responseType").equals("userAnnouncement")){
             Platform.runLater(() -> {
                 addPlayers(responseJSON.getJSONArray("userList"));
-                if (responseJSON.getBoolean("isStartable")){
+                if (responseJSON.getBoolean("isStartable") || payload.getBoolean("isStartable")){
                     startButton.setDisable(false);
                 }
             });
@@ -55,6 +55,11 @@ public class RoomLobbyMenuController extends MenuController {
             Platform.runLater(() -> {
                 ownerID = responseJSON.getInt("ownerID");
                 startButton.setVisible(ownerID == UserConfiguration.userID);
+            });
+        }
+        else if(responseJSON.getString("responseType").equals("interruptCounter")){
+            Platform.runLater(() -> {
+                roomName.setText((payload.getString("name")));
             });
         }
         else if(responseJSON.getString("responseType").equals("startGame")){
